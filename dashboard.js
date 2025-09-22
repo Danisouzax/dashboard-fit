8a79e8822d4499a580c547cc8b86d851b536f313
Dani Souza <daniele.carvalho04@gmail.com>
2025-09-22 15:51:31 -0300

    atualização API

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,6 @@
 const CLIENT_ID = "32608600853-8h0s2q50bo9mfs09jr8vb4eh7eoqrcmd.apps.googleusercontent.com";
-const SCOPES = `
-  https://www.googleapis.com/auth/fitness.activity.read
-  https://www.googleapis.com/auth/fitness.body.read
-`;
+const SCOPES = "https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read";
+
 
 let accessToken = null;
 
