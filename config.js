b8affb1fc61097208d99e9367238adc7a3a1d8ec
Dani Souza <daniele.carvalho04@gmail.com>
2025-09-23 12:25:26 -0300

    atualização API

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+export const CLIENT_ID = "32608600853-8h0s2q50bo9mfs09jr8vb4eh7eoqrcmd.apps.googleusercontent.com";
+export const SCOPES = "https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read";
