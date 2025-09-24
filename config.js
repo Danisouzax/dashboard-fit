e0d779e219287e74b58627e40c808b82fa2b5312
Dani Souza <daniele.carvalho04@gmail.com>
2025-09-24 08:14:38 -0300

    Atalização de API

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // config.js
-export const CLIENT_ID = "32608600853-8h0s2q50bo9mfs09jr8vb4eh7eoqrcmd.apps.googleusercontent.com";
+export const CLIENT_ID = "296563341491-968af535qt41a0fcr31lm6vcfumd6e69.apps.googleusercontent.com";
 export const SCOPES = "https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read";
+
